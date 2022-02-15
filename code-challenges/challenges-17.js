d89529e89243c85ab8ffb04980fc92c58784031b
Manal Albahar <manal.albahar@gmail.com>
2022-02-15 23:51:03 +0200

    Fixed challenge 17 pull

--- code-challenges/challenges-17.js
+++ code-challenges/challenges-17.js
@@ -100,7 +100,7 @@ const isPalindrome = (str) => {
 
 const samePattern = (str, arr) => {
     // write your code here
-  
+
     let keys = {};
     let res = true;
     for (let i = 0; i < str.length; i++) {
@@ -113,7 +113,7 @@ const samePattern = (str, arr) => {
     }
     return res;
 }
-     
+
 
 // -------------------------------------------------------------------------------------------------------
 
